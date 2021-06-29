c7e651bf686bfe9d197cf71a9838289601013b32
Mariia Gorbunova <maryks44m@yandex.ru>
2021-06-29 22:51:37 +0300

    the exercise number 1: calculating the area of ​​a triangle

---
+++ python_part_1/exe1_1.py
@@ -0,0 +1,7 @@
+#the exercise number 1 https://stepik.org/lesson/5047/step/1?auth=login&unit=1086
+
+a,b,c = int(input()),int(input()),int(input())
+p = (a + b + c)/2
+S = (p*(p-a)*(p-b)*(p-c))**0.5
+print(S)
+
