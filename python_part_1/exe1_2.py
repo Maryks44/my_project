a343b26aa822fc68cc2e82e2fc1458f33dc1b359
Mariia Gorbunova <maryks44m@yandex.ru>
2021-06-29 23:04:32 +0300

    the exercise number 2: condition if/elif/else

---
+++ python_part_1/exe1_2.py
@@ -0,0 +1,7 @@
+#the exercise number 2 https://stepik.org/lesson/5047/step/2?auth=login&unit=1086
+
+a = int(input())
+if -15 < a <= 12 or 14 < a < 17 or a >= 19:
+    print("True")
+else:
+    print("False ")
