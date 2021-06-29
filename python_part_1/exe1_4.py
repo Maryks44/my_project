d8091adaad5655b858f86b02241c176d8b4b948c
Mariia Gorbunova <maryks44m@yandex.ru>
2021-06-29 23:51:59 +0300

    the exercise number 4: calculating the area of ​​a round, rectangle, triangle

---
+++ python_part_1/exe1_4.py
@@ -0,0 +1,13 @@
+#the exercise number 3 https://stepik.org/lesson/5047/step/4?auth=login&unit=1086
+
+name = input()
+if name == 'треугольник':
+    a,b,c = float(input()),float(input()),float(input())
+    p = (a + b + c)*0.5
+    print((p*(p-a)*(p-b)*(p-c))**0.5)  
+elif name == 'прямоугольник':
+    a,b = float(input()),int(input())
+    print(a*b)
+elif name == 'круг':
+    r = float(input())
+    print(3.14*(r**2))
